b6e899853a44f336d101411beccdf56f07f53e30
Stefan Bogdanel <bogdan.izdrail@gmail.com>
2024-11-21 23:19:46 +0000

    Added content

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/",
-    "revision": "0.6uv8n8vlbm8"
+    "revision": "0.qo7lcguf4lg"
   }], {
     "directoryIndex": "index.html"
   });
